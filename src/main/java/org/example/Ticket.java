5dc4593342bbf9389be1419cea8a2e59e9536efe
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-01 21:07:32 +0530

    Create Ticket.java

---
+++ src/main/java/org/example/Ticket.java
@@ -0,0 +1,14 @@
+package org.example;
+public class Ticket {
+    private final String ticketId;
+
+    public Ticket(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getTicketId() {
+        return ticketId;
+    }
+}
+
+
