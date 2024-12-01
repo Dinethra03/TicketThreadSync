e19d120b5ff89115a99b8ccb122048edd8a2b5ac
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-01 21:09:07 +0530

    Create Configuration.java

---
+++ src/main/java/org/example/Configuration.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Configuration {
+    private final int totalTickets;
+    private final int maxCapacity;
+    private final int releaseRate;
+    private final int retrievalRate;
+
+    public Configuration(int totalTickets, int maxCapacity, int releaseRate, int retrievalRate) {
+        this.totalTickets = totalTickets;
+        this.maxCapacity = maxCapacity;
+        this.releaseRate = releaseRate;
+        this.retrievalRate = retrievalRate;
+    }
+
+    public int getTotalTickets() {
+        return totalTickets;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public int getReleaseRate() {
+        return releaseRate;
+    }
+
+    public int getRetrievalRate() {
+        return retrievalRate;
+    }
+}
+
+
