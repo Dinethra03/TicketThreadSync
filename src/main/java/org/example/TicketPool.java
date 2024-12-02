9431ff4d3d595544caf5b3dcd2501cbcf024ccd3
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-03 00:22:04 +0530

    Change TicketPool.java

--- src/main/java/org/example/TicketPool.java
+++ src/main/java/org/example/TicketPool.java
@@ -6,21 +6,19 @@ import java.util.concurrent.BlockingQueue;
 
 public class TicketPool {
     private final BlockingQueue<Ticket> tickets;
-    private final int maxCapacity;
 
     public TicketPool(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
         this.tickets = new ArrayBlockingQueue<>(maxCapacity);
     }
 
     public void addTickets(Ticket ticket) throws InterruptedException {
-        tickets.put(ticket); // This is a blocking call that will wait until there is space in the queue
-        System.out.println("Added: " + ticket.getTicketId());
+        tickets.put(ticket); // Blocks if the queue is full
+        System.out.println("[TicketPool] Added: " + ticket.getTicketId());
     }
 
     public Ticket removeTicket() throws InterruptedException {
-        Ticket ticket = tickets.take(); // This is a blocking call that will wait until a ticket is available
-        System.out.println("Removed: " + ticket.getTicketId());
+        Ticket ticket = tickets.take(); // Blocks if the queue is empty
+        System.out.println("[TicketPool] Removed: " + ticket.getTicketId());
         return ticket;
     }
 
