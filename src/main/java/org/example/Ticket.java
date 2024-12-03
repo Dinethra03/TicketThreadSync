eee3a4671678a44ef5be7edf816a261b4086f19f
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-03 13:44:28 +0530

    Change Ticket.java

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -1,4 +1,5 @@
 package org.example;
+
 public class Ticket {
     private final String ticketId;
 
@@ -9,6 +10,18 @@ public class Ticket {
     public String getTicketId() {
         return ticketId;
     }
-}
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Ticket ticket = (Ticket) obj;
+        return ticketId.equals(ticket.ticketId);
+    }
+
+    @Override
+    public int hashCode() {
+        return ticketId.hashCode();
+    }
+}
 
