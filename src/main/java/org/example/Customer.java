782d99b4230fd21b5c7f082b2674aed507153b31
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-07 01:49:35 +0530

    change Customer.java

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -6,11 +6,11 @@ import org.apache.logging.log4j.Logger;
 public class Customer implements Runnable {
     private static final Logger logger = LogManager.getLogger(Customer.class);
     private final TicketPool ticketPool;
-    private final int retrievalInterval;
+    private final int customerRetrievalRate;
 
-    public Customer(TicketPool ticketPool, int retrievalInterval) {
+    public Customer(TicketPool ticketPool, int customerRetrievalRate) {
         this.ticketPool = ticketPool;
-        this.retrievalInterval = retrievalInterval;
+        this.customerRetrievalRate = customerRetrievalRate;
     }
 
     @Override
@@ -18,8 +18,9 @@ public class Customer implements Runnable {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Ticket ticket = ticketPool.removeTicket();
+                System.out.println("[ " +  Thread.currentThread().getName() +  "] Bought ticket: " + ticket.getTicketId());
                 logger.info("Customer retrieved ticket: " + ticket.getTicketId());
-                Thread.sleep(retrievalInterval);
+                Thread.sleep(customerRetrievalRate * 1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
                 logger.error("Customer interrupted while retrieving tickets.");
