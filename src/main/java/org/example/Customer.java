032e7e36bc4445c53486fa8e784dede84ca570ea
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-08 11:06:52 +0530

    Change Customer.java

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javafx.application.Platform;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -7,10 +8,12 @@ public class Customer implements Runnable {
     private static final Logger logger = LogManager.getLogger(Customer.class);
     private final TicketPool ticketPool;
     private final int customerRetrievalRate;
+    private final Label ticketCountLabel; // UI Label
 
-    public Customer(TicketPool ticketPool, int customerRetrievalRate) {
+    public Customer(TicketPool ticketPool, int customerRetrievalRate, Label ticketCountLabel) {
         this.ticketPool = ticketPool;
         this.customerRetrievalRate = customerRetrievalRate;
+        this.ticketCountLabel = ticketCountLabel;
     }
 
     @Override
@@ -18,13 +21,14 @@ public class Customer implements Runnable {
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Ticket ticket = ticketPool.removeTicket();
-                System.out.println("[ " +  Thread.currentThread().getName() +  "] Bought ticket: " + ticket.getTicketId());
-                logger.info("Customer retrieved ticket: " + ticket.getTicketId());
+
+                // Update UI
+                Platform.runLater(() -> ticketCountLabel.setText("Tickets in Pool: " + ticketPool.getTicketCount()));
+
+                logger.info("[ " + Thread.currentThread().getName() + "] Bought ticket: " + ticket.getTicketId());
                 Thread.sleep(customerRetrievalRate * 1000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                System.out.println("[ " + Thread.currentThread().getName() + " ] interrupted while retrieving tickets.");
-                logger.error("[ " + Thread.currentThread().getName() +  " ] interrupted while retrieving tickets.");
             }
         }
     }
