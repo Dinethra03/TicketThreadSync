1058ebdd6936fada9ac5896eafffd22e3ab7aaf1
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-08 18:03:47 +0530

    Change Customer.java

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -24,6 +24,7 @@ public class Customer implements Runnable {
 
     @Override
     public void run() {
+        // Run ticket-buying logic in this thread
         while (!Thread.currentThread().isInterrupted()) {
             try {
                 Ticket ticket = ticketPool.removeTicket(); // Retrieve a ticket from the pool
@@ -37,7 +38,7 @@ public class Customer implements Runnable {
 
                 Thread.sleep(customerRetrievalRate * 1000); // Sleep before retrieving the next ticket
             } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
+                Thread.currentThread().interrupt(); // Handle thread interruption
             }
         }
     }
