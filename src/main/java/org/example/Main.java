63646477d8a065428daee7cc99f124c72bc37341
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-10 20:00:11 +0530

    Add comments to Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example; //Declare the class's package
 
 import java.util.Scanner;
 
@@ -6,15 +6,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose mode: 'cli' or 'gui'");
+        System.out.println("Choose mode: 'cli' or 'gui'"); //Prompting the user to select between CLI and GUI modes
         String mode = scanner.nextLine().toLowerCase();
 
-        if (mode.equals("cli")) {
+        if (mode.equals("cli")) { //Checking whether the user selected "cli"
             // Run the CLI logic
-            new CLIApp().run();
+            new CLIApp().run(); //Craeting and launching CLIApp (CLI-based logic)
         } else if (mode.equals("gui")) {
             // Run the GUI logic
-            MainApp.main(args); // Launch JavaFX application
+            MainApp.main(args); // Launching the JavaFx application (GUI-based logic)
         } else {
             System.out.println("Invalid mode. Please restart and choose 'cli' or 'gui'.");
         }
