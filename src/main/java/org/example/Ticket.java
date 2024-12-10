efa617d70e332bc515973ad6e96f0a20a584b69a
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-10 22:58:13 +0530

    Add comments to Ticket.java

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -1,12 +1,15 @@
 package org.example;
-public class Ticket {
-    private final String ticketId;
 
+public class Ticket {
+    private final String ticketId; //A unique identification for the ticket, declared final because it should not change
+
+    //The constructor will initialize the ticket with its unique identifier
     public Ticket(String ticketId) {
-        this.ticketId = ticketId;
+        this.ticketId = ticketId; //Assign the given ticketId to the ticket
     }
 
+    //Getter method for retrieving the ticket's unique identification
     public String getTicketId() {
-        return ticketId;
+        return ticketId; //Return the ticketID connected with this ticket
     }
 }
