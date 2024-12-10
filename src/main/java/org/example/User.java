336ad0a475508d73e797a37c0cc4ce13b8977d21
Dinethra03 <samarakoondinethra@gmail.com>
2024-12-10 20:00:50 +0530

    Add comments to User.java

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -3,8 +3,9 @@ package org.example;
 public class User {
     private String username;
     private String password;
-    private String userType; // Can be "Customer" or "Vendor"
+    private String userType; // Declare a private field to store the user's type (customer or vendor)
 
+    //Constructor to create a new user object with the specified username, password and user type
     public User(String username, String password, String userType) {
         this.username = username;
         this.password = password;
@@ -23,6 +24,8 @@ public class User {
         return userType;
     }
 
+
+    //Override the toString method and provide a string representation of the user object
     @Override
     public String toString() {
         return "User{" +
